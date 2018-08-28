2eb175a98cff32c57ab4837fa9df45837a594cef
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-08-28 18:11:17 +0100

    CafeTheme

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+	Name = "Cafe Theme",
+	Author = "Marcos Aidos",
+	Website = "https://startbootstrap.com/template-overviews/business-casual/",
+	Version = "1.0.0-beta3",
+	Description = "A theme adapted for OrchardCore"
+)]
